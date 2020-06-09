dd9047cf6e60abd5c14750fc510dd01889b798c7
1-EXON <exon.indiv@gmail.com>
2020-06-09 18:05:35 +0900

    Upload

---
+++ Discord Webhook/Discord Webhook/DiscordWebhook.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace Discord_Webhook
+{
+    partial class DiscordWebhook
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.channel = new System.Windows.Forms.TextBox();
+            this.name = new System.Windows.Forms.TextBox();
+            this.image = new System.Windows.Forms.TextBox();
+            this.content = new System.Windows.Forms.TextBox();
+            this.Send = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // channel
+            // 
+            this.channel.Location = new System.Drawing.Point(12, 67);
+            this.channel.Name = "channel";
+            this.channel.Size = new System.Drawing.Size(738, 21);
+            this.channel.TabIndex = 0;
+            // 
+            // name
+            // 
+            this.name.Location = new System.Drawing.Point(226, 133);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(260, 21);
+            this.name.TabIndex = 1;
+            // 
+            // image
+            // 
+            this.image.Location = new System.Drawing.Point(12, 194);
+            this.image.Name = "image";
+            this.image.Size = new System.Drawing.Size(738, 21);
+            this.image.TabIndex = 2;
+            // 
+            // content
+            // 
+            this.content.Location = new System.Drawing.Point(12, 260);
+            this.content.Name = "content";
+            this.content.Size = new System.Drawing.Size(738, 21);
+            this.content.TabIndex = 3;
+            // 
+            // Send
+            // 
+            this.Send.Location = new System.Drawing.Point(318, 351);
+            this.Send.Name = "Send";
+            this.Send.Size = new System.Drawing.Size(75, 23);
+            this.Send.TabIndex = 4;
+            this.Send.Text = "Send";
+            this.Send.UseVisualStyleBackColor = true;
+            this.Send.Click += new System.EventHandler(this.Send_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(310, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Webhook Link";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(328, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(310, 179);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Profile Image Link";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(319, 245);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 12);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Content";
+            // 
+            // DiscordWebhook
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Send);
+            this.Controls.Add(this.content);
+            this.Controls.Add(this.image);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.channel);
+            this.Name = "DiscordWebhook";
+            this.Text = "Discord Webhook";
+            this.Load += new System.EventHandler(this.DiscordWebhook_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox channel;
+        private System.Windows.Forms.TextBox name;
+        private System.Windows.Forms.TextBox image;
+        private System.Windows.Forms.TextBox content;
+        private System.Windows.Forms.Button Send;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Discord Webhook/Discord Webhook/DiscordWebhook.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Discord_Webhook
+{
+    public partial class DiscordWebhook : Form
+    {
+        public DiscordWebhook()
+        {
+            InitializeComponent();
+        }
+
+        private void DiscordWebhook_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Send_Click(object sender, EventArgs e)
+        {
+            using (DcWebHook dcWeb = new DcWebHook())
+            {
+                dcWeb.ProfilePicture = image.Text;
+                dcWeb.UserName = name.Text;
+                dcWeb.WebHook = channel.Text;
+                dcWeb.SendMessage(content.Text);
+            }
+        }
+    }
+}
